0d72be33858f9ea989255aa4de9b0d792a3ec916
Wang Shen <kongtiaowangshen@gmail.com>
2017-10-16 12:56:39 -0400

    react

---
+++ mysite/assets/js/client.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import Layout from "./components/Layout";
+
+ReactDOM.render(
+  <h1>Hello, world</h1>,
+  document.getElementById('app')
+);
